91ab8ef08dc89f0fd27609c9d1e770cdd9f85763
Calum O'Neil <calum.oneil@gmail.com>
2014-05-19 19:59:09 +1200

    Initialize repository.
    Add a new project called "beacon", a simple app to track the status of what each person is doing in a small team.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beacon::Application.config.secret_token = 'bca3ac96996c8403aa0ebe27dbe7225171473ce4e83cade8daae7ecfd5d78a37e7582b6dc913c38c3d967e5e7255d8eec7b5826680b4e78587884fa28e04f16d'
