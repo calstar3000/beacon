e321e85fd1ce2885b49d93bb717698074521728c
Calum O'Neil <calum.oneil@gmail.com>
2014-05-20 23:01:48 +1200

    Added a full page title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# returns the full title of a page
+	def full_title(page_title)
+		base_title = "Beacon"
+		if (page_title.empty?)
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
+
 end
