08756cf25948a32cd004e8ce571cc045de89eb17
Calum O'Neil <calum.oneil@gmail.com>
2014-05-25 16:22:13 +1200

    Add logging in and out functionality. Add mega menu to hold account links.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
+	before_create :create_remember_token
 
 	attr_accessible :email, :name, :password, :password_confirmation
 
@@ -17,5 +18,20 @@ class User < ActiveRecord::Base
   									uniqueness: { case_sensitive: false }
 
 	has_secure_password
-	validates :password, length: { minimum: 6 }	
+	validates :password, length: { minimum: 6 }
+
+	def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+	private
+		def create_remember_token
+			token = User.new_remember_token
+			digest = User.digest(token)
+			self.remember_token = digest
+		end
 end
