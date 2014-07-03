3d775b171e507f8509e288649ed5a40cc36f14f4
Calum O'Neil <calum.oneil@gmail.com>
2014-07-03 22:08:42 +1200

    Add user following and proper status feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,6 +48,10 @@ class User < ActiveRecord::Base
   	self.relationships.find_by_followed_id(other_user.id).destroy
   end
 
+  def status_feed
+    Status.from_users_followed_by(self)
+  end
+
 	private
 		def create_remember_token
 			token = User.new_remember_token
